c9d25c9a534fa86671d172f0973f40f1331f79a3
FrankoH298 <francoh298@gmail.com>
2020-05-14 20:07:48 -0300

    Agregados models

--- core/biblio/admin.py
+++ core/biblio/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from biblio.models import *
 
 # Register your models here.
+admin.site.register(Autor,)
+admin.site.register(Libro,)
--- core/biblio/models.py
+++ core/biblio/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Autor(models.Model):
+    Nombre = models.CharField(max_length=50)
+    Codigo = models.IntegerField(primary_key=True)
+
+    def __str__(self):
+        return ("{} : {}".format(self.Codigo, self.Nombre))
+
+class Libro(models.Model):
+    Codigo = models.IntegerField(primary_key=True)
+    Titulo = models.CharField(max_length=30)
+    Paginas = models.IntegerField()
+    Editorial = models.CharField(max_length=40)
+    Autor = models.ForeignKey('Autor', on_delete=models.CASCADE, null=True,)
+
+    def __str__(self):
+        return ("{} : {}".format(self.Codigo, self.Titulo))
