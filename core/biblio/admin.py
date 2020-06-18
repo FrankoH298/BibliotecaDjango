de87e9a7f25b38c8c522959f78b77bf35cc8a96d
FrankoH298 <francoh298@gmail.com>
2020-06-18 12:21:03 -0300

    Agregado fieldsets

--- core/biblio/admin.py
+++ core/biblio/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
 from biblio.models import *
 
+class UserAdmin(admin.ModelAdmin):
+    
+    fieldsets = (
+        ('Información Personal', {
+            'fields': ('Nombre', 'Direccion', 'Telefono',)
+        }),
+        ('Detalle de alquileres', {
+            'fields': ('Ejemplares',)
+        }),
+    )
+
+    list_display = ['Nombre', 'Telefono']
+
 # Register your models here.
 admin.site.register(Autor,)
 admin.site.register(Libro,)
 admin.site.register(Ejemplar,)
-admin.site.register(Usuario,)
+admin.site.register(Usuario, UserAdmin)
